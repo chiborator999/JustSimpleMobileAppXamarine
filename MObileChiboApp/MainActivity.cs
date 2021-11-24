d3b25ab0fe30f31694bc6a8e4b4c021f8f32bd2f
chiborator999 <dsspasov@gmail.com>
2021-11-24 16:37:25 +0200

    add startup files

---
+++ MObileChiboApp/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace MObileChiboApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView txtBox;
+
+        int number;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            txtBox = FindViewById<TextView>(Resource.Id.txtBox);
+
+            FindViewById<Button>(Resource.Id.btnUp).Click += (e, o) => txtBox.Text = (number++).ToString();
+            FindViewById<Button>(Resource.Id.btnDown).Click += (e, o) => txtBox.Text = (number--).ToString();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
